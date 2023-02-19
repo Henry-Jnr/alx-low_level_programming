ada5d3f816d441c745c6cd1a00014e1eb0335d26
Henry-Jnr <dadrimz552@gmail.com>
2023-02-20 00:59:47 +0100

    My first C commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf ("Alx Software Engineering Program is Great!");
-return (0);
+    return (0);
 }
