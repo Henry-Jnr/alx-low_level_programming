56a21aaad5aa8d7075a6328ae01d524a4a413c5a
Henry-Jnr <dadrimz552@gmail.com>
2023-02-21 14:42:39 -0800

    My first C commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 /*A program demonstrating the printf function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
