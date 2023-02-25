c4f096319564cdfed4222235ae6d85f509706ac7
Henry-Jnr <dadrimz552@gmail.com>
2023-02-25 04:26:59 -0800

    C if_else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Program to print base 16 numbers in lower case
+ */
+int main(void)
+{
+	int a;
+	char letter;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+
+	for (letter = 0; letter < 'g'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
