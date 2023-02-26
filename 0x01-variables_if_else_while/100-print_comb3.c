c560876164f7c676e819e5eb741d5fc917e9c2d0
Henry-Jnr <dadrimz552@gmail.com>
2023-02-25 16:22:50 -0800

    C if_else

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,21 @@
 int main(void)
 {
 	int a, b;
-
 	for (a = 0; a < 9; a++)
-{
-	for (b = 1; b < 10; b++)
 	{
-		putchar((a % 10) + '0');
-		putchar((b % 10) + '0');
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+				if (a == 8 && b == 9)
+ 					continue;
 
-		if (a == 8 && b == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+				putchar(',');
+				putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+
+
+	putchar('\n');
+	return (0);
 }
